5c1a7f0ee685d04cb4aa66a2dd95ee76ca392229
Ken Yoshihara <yshrkn@gmail.com>
2019-12-29 23:52:48 +0900

    leared props of components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
 import React from 'react';
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <label htmlFor='bar'>bar</label>
-//       <input id='bar' type='text' onChange={() => {console.log("I am clicked!")}} />
-//     </React.Fragment>
-//   );
-// }
-
-// class comp[onent
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor='bar'>bar</label>
-//         <input id='bar' type='text' onChange={() => {console.log("I am clicked!")}} />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-// functional component
 const App = () => {
+  const profiles = [
+    { name: 'Taro', age: 10,},
+    { name: 'Hanako', age: 5,},
+    { name: 'NoName' },
+  ];
+
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => <User name={profile.name} age={profile.age} key={index} />)
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old.</div>
+}
+
+User.defaultProps = {
+  age: 1,
 }
 
 export default App;
